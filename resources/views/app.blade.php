11ea3d0f8f79bbabc75d2cadff895118743ae9d2
Jannes Behrens <jannescb@me.com>
2022-02-02 13:58:18 +0100

    wip

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
     <head>
+        @csrf
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
