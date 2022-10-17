57ad0a211fadaffdc43782fa03636513abc6a856
Jannes Behrens <jannescb@me.com>
2022-10-17 11:03:53 +0200

    make buergertest grund mandatory

--- resources/js/modules/validation/index.ts
+++ resources/js/modules/validation/index.ts
@@ -1,6 +1,5 @@
-import { computed, watch, ref } from "vue";
+import { computed, watch, ref } from 'vue';
 import { form, getAttr } from '@/modules/booking';
-
 
 export const validateEmail = email => {
     return String(email)
@@ -11,9 +10,11 @@ export const validateEmail = email => {
 };
 export const emailConfirmed = computed(() => {
     if (validateEmail(form.email) && validateEmail(form.email_confirmation)) {
-        return form.email?.toLowerCase() == form.email_confirmation?.toLowerCase();
+        return (
+            form.email?.toLowerCase() == form.email_confirmation?.toLowerCase()
+        );
     }
-    return true
+    return true;
 });
 export const emailState = computed(() => {
     if (!emailConfirmed.value) {
@@ -43,7 +44,6 @@ export const stageEmailValid = computed(() => {
     return form.email?.toLowerCase() == form.email_confirmation?.toLowerCase();
 });
 
-
 export const givenNameState = computed(() => {
     if (form.errors['positions.0.attendee_name_parts.given_name']) {
         return false;
@@ -51,15 +51,15 @@ export const givenNameState = computed(() => {
 
     let attribute = form.positions[0].attendee_name_parts.given_name;
 
-    if (attribute == null ||  attribute == '') {
+    if (attribute == null || attribute == '') {
         return null;
     }
 
-    if(containsDigits(attribute)){
+    if (containsDigits(attribute)) {
         return false;
     }
 
-    if (attribute?.length < 2 ) {
+    if (attribute?.length < 2) {
         return null;
     }
     return true;
@@ -71,7 +71,7 @@ export const familyNameState = computed(() => {
 
     let attribute = form.positions[0].attendee_name_parts.family_name;
 
-    if(containsDigits(attribute)){
+    if (containsDigits(attribute)) {
         return false;
     }
 
@@ -83,20 +83,20 @@ export const streetNoState = computed(() => {
     // the street is valid if
     let street = getAttr('street-no').answer;
 
-    // ... it is not emtpy 
-    if(street?.length < 1 || street == null || street == '') {
+    // ... it is not emtpy
+    if (street?.length < 1 || street == null || street == '') {
         return null;
     }
-    
+
     // ... it contains a digit
-    if(!containsDigits(street)){
+    if (!containsDigits(street)) {
         return false;
     }
     // ... it contains a number
-    if(!containsLetter(street)){
+    if (!containsLetter(street)) {
         return false;
     }
-    
+
     // ... it is at least 5 chars long (e.g. "Tal 1")
     if (street?.length < 5) {
         return false;
@@ -105,27 +105,26 @@ export const streetNoState = computed(() => {
     return true;
 });
 
-
-export const zipState = (zip) => {
+export const zipState = zip => {
     if (zip == null) {
         return;
     }
     return zip.toString().length >= 3;
 };
-export const cityState =(city) => {
+export const cityState = city => {
     if (city == null) {
         return;
     }
-    
+
     return city.toString().length >= 2;
 };
-import { zip, city, zipCity} from '@/modules/booking/state';
+import { zip, city, zipCity } from '@/modules/booking/state';
 export const zipCityState = ref(null);
 
 watch(
     () => zipCity.value,
     value => {
-        zipCityState.value =  zipState(zip.value) && cityState(city.value)
+        zipCityState.value = zipState(zip.value) && cityState(city.value);
     }
 );
 
@@ -138,17 +137,16 @@ export const phoneState = computed(() => {
     let attribute = getAttr('phone').answer;
 
     // ... has at least 7 digits
-    if(attribute?.toString().match(/[0-9]{7,}/g) == null){
-        return false
-    } 
+    if (attribute?.toString().match(/[0-9]{7,}/g) == null) {
+        return false;
+    }
     // ... has no letters
-    if(containsLetter(attribute)){
-        return false
-    } 
+    if (containsLetter(attribute)) {
+        return false;
+    }
 
-    return true
+    return true;
 });
-
 
 const validDayNumbers = [
     '01',
@@ -198,7 +196,7 @@ const validMonthNumbers = [
     '12',
 ];
 
-export const dayState = (day) => {
+export const dayState = day => {
     if (day?.toString().length == 2) {
         return validDayNumbers.includes(day) ? true : false;
     }
@@ -206,62 +204,65 @@ export const dayState = (day) => {
         return false;
     }
 };
-export const monthState= (month) => {
+export const monthState = month => {
     if (month?.toString().length == 2) {
         return validMonthNumbers.includes(month) ? true : false;
     }
-    if (
-        month?.toString().length > 2 ||
-        month?.toString().length == 1
-    ) {
+    if (month?.toString().length > 2 || month?.toString().length == 1) {
         return false;
     }
 };
-export const yearState = (year) => {
+export const yearState = year => {
     if (year?.toString().length == 4) {
-        return parseInt(year) > 1900 && parseInt(year) < 2020
-            ? true
-            : false;
+        return parseInt(year) > 1900 && parseInt(year) < 2020 ? true : false;
     }
-    if (
-        year?.toString().length != 4 &&
-        year?.toString().length > 0
-    ) {
+    if (year?.toString().length != 4 && year?.toString().length > 0) {
         return false;
     }
 };
 
-import { day, month, year, date} from '@/modules/booking/state';
+import { day, month, year, date } from '@/modules/booking/state';
 export const birthdateState = ref(null);
 
 watch(
     () => date.value,
     value => {
-        birthdateState.value =  dayState(day.value) && monthState(month.value) && yearState(year.value)
+        birthdateState.value =
+            dayState(day.value) &&
+            monthState(month.value) &&
+            yearState(year.value);
     }
 );
-
 
 /**
  * The form stage is valid
  */
-export const stageFormValid = computed(()=>{
-    return givenNameState.value && familyNameState.value && streetNoState.value && phoneState.value && zipCityState.value && birthdateState.value
+export const stageFormValid = computed(() => {
+    return (
+        givenNameState.value &&
+        familyNameState.value &&
+        streetNoState.value &&
+        phoneState.value &&
+        zipCityState.value &&
+        birthdateState.value &&
+        (getAttr('buergertest-grund')
+            ? getAttr('buergertest-grund').answer != null
+            : true)
+    );
 });
 
-export const hasAtLeastNDigits = (val: any, digits: number ) => {
-    let rule = `[0-9]{${digits},}`
-    let regex = new RegExp(rule,"g");
-    return val?.toString().match(regex) != null
-       
-}
+export const hasAtLeastNDigits = (val: any, digits: number) => {
+    let rule = `[0-9]{${digits},}`;
+    let regex = new RegExp(rule, 'g');
+    return val?.toString().match(regex) != null;
+};
 
 export const containsDigits = (val: any) => {
     let matches = val?.toString().match(/\d+/g);
     return matches != null;
-}
+};
 
 export const containsLetter = (val: any) => {
     let matches = val?.toString().match(/[a-zA-ZßöäüÖÄÜ]+/g);
     return matches != null;
-}
+};
